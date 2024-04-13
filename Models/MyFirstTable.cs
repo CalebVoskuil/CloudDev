1ad339ddf19ebb7dfb6db6baec31aafd997799ed
Caleb Brent Voskuil <caleb.voskuil13@gmail.com>
2024-04-13 03:09:29 +0200

    Added connection to my sql database on azure.

---
+++ Models/MyFirstTable.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace TestForWebApp.Models
+{
+
+    public class MyFirstTable : Controller
+    {
+        public static string con_string = "Server=tcp:cloud-dev-server.database.windows.net,1433;Initial Catalog=CLOUD_DEV;Persist Security Info=False;User ID=CalebVoskuil;Password=##LenovoSilver!1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection(con_string);
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
