c111270a484ee442c92cdd081c465646c181ca0f
Caleb Voskuil <caleb.voskuil@gmail.com>
2024-04-18 10:15:54 +0200

    added basic SQL con stuff

---
+++ Models/UserTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Assignment1.Models
+{
+    public class UserTable
+    {
+        public static string con_string = "Server=tcp:cloud-dev-server.database.windows.net,1433;Initial Catalog=CLOUD_DEV;Persist Security Info=False;User ID=CalebVoskuil;Password=##LenovoSilver!1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection(con_string);
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Surname { get; set; }
+       
+        public int insert_User(UserTable m) 
+        {
+            string sql = " INSERT INTO UserTable userName, userSurname, userEmail (@Name, @Surname, @Email)";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Name", m.Name);
+            cmd.Parameters.AddWithValue("@Surname", m.Surname);
+            cmd.Parameters.AddWithValue("@Email", m.Email);
+            con.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            con.Close();
+
+            return rowsAffected;
+
+
+        }
+
+    }
+}
